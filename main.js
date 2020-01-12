8922f4f5408e53b2cea8656aa0ab47d3edc14cc1
eric sciple <ericsciple@users.noreply.github.com>
2020-01-11 23:36:29 -0500

    setup runner

---
+++ main.js
@@ -0,0 +1,45 @@
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const github = require('@actions/github')
+
+async function run() {
+  // Inputs
+  const repository = 'actions/runner'
+  const ref = core.getInput('ref') || 'master'
+  const token = core.getInput('token', {required: true})
+
+  // Git clone
+  await exec.exec('git', ['clone', `https://github.com/${repository}`])
+
+  // Chdir
+  const repositoryName = repository.split('/')[1]
+  process.chdir(`${repositoryName}/src`)
+  await exec.exec('pwd')
+
+  // Git checkout
+  await exec.exec(git, ['checkout', ref])
+
+  // Build
+  await exec.exec('./dev.sh', ['layout'])
+
+  // Chdir
+  process.chdir('../_layout')
+  await exec.exec('pwd')
+
+  // Config
+  await exec.exec(
+    './config.sh',
+    [
+      '--unattended',
+      '--replace',
+      '--url',
+      `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}`,
+      '--token',
+      token
+    ])
+
+  // Run
+  await exec.exec('./config.sh')
+}
+
+run()
